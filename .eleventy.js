a1f7e0911b4a4311468ce1d2fa20818cebb02dca
Pico <>
2020-11-09 12:07:22 +0000

    configured basic template and html minifier

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,18 @@
+const htmlmin = require("html-minifier");
+// const sass = require("sass");
+
 module.exports = function(eleventyConfig) {
 
-    eleventyConfig.addPassthroughCopy("src/js");
+    eleventyConfig.setTemplateFormats([
+        "njk",
+        "css",
+        "jpg",
+        "png"
+    ]);
 
-    eleventyConfig.setBrowserConfig({
+    eleventyConfig.addWatchTarget("./src/scss");
+
+    eleventyConfig.setBrowserSyncConfig({
         https: {
             key: '../localhost.key',
             cert: '../localhost.cert'
@@ -11,9 +21,38 @@ module.exports = function(eleventyConfig) {
         open: "local"        
     });
 
+    // eleventyConfig.addTransform("sass", function(content, outputhPath) {
+    //     if( outputhPath.endsWith(".scss") ) {
+    //         let rendered = sass.render({
+    //             data: content,
+    //             includePaths: ["/scss"],
+    //             sourceMap: true,
+    //             sourceMapEmbed: true,
+
+    //         });
+    //         return rendered;
+    //     }
+
+    //     return content;
+    // });
+
+    eleventyConfig.addTransform("htmlmin", function(content, outputhPath) {
+        if( outputhPath.endsWith(".html") ) {
+            let minified = htmlmin.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true
+            });
+            return minified;
+        }
+
+        return content;
+    });
+
     return {
         dir: {
             input: "src",
+            data: "_includes",
             output: "dist"
         }
     };
