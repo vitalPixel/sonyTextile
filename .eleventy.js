76219b43f0f2c33fb6b209587c52b2b00a6d99dc
Pico <>
2020-11-12 16:40:52 +0000

    added redirect rule to browsersync for local env

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 const htmlmin = require("html-minifier");
-const sass = require("sass");
+const fs = require("fs");
 
 module.exports = function(eleventyConfig) {
 
@@ -18,7 +18,21 @@ module.exports = function(eleventyConfig) {
             cert: '../localhost.cert'
         },
         browser: "firefox",
-        open: "local"        
+        open: "local",
+        callbacks: {
+            ready: function(err, bs) {
+      
+              bs.addMiddleware("*", (req, res) => {
+                const content_404 = fs.readFileSync('dist/404.html');
+                // Provides the 404 content without redirect.
+                res.write(content_404);
+                // Add 404 http status code in request header.
+                // res.writeHead(404, { "Content-Type": "text/html" });
+                res.writeHead(404);
+                res.end();
+              });
+            }
+          }
     });
 
     eleventyConfig.addTransform("htmlmin", function(content, outputhPath) {
